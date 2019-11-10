3a972730d75ae2839a911e857077d4c1b71f8848
Kumar Pradumn <52545723+kumar0912@users.noreply.github.com>
2019-11-11 04:08:07 +0530

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -8,14 +8,29 @@ struct Pair
 };
 
 struct customer
-    { 
+{ 
       char email[51];
       char password[51];
       char name[20];
-      int user_type;
+      const int type=0;
       long long contact_number;
       long long credit_balance;
       long long wallet_balance;
       bool defaulter;
       Pair transaction_history[20];
-    };    
+};  
+
+struct shopkeeper
+{
+   char email[51];
+   char password[51];
+   char name[20];
+   const int type=1;
+   char shop_name[30];
+   long long account_number;
+   long long pending_amount;
+   Pair transaction_history[20];
+   long long holiday[366];  
+};
+
+

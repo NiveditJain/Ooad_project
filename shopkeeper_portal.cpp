a4b4ce326e8433471842af63912ccbad96ab3a65
rohith-nagamalla <nagamalla.1@iitj.acin>
2019-11-16 07:29:03 +0530

    updated shop-keeper

--- shopkeeper_portal.cpp
+++ shopkeeper_portal.cpp
@@ -4,17 +4,74 @@
 using namespace std;
 #include "database.cpp"
 #include "shopkeeper.cpp"
+#include "shop.cpp"
 
-void home_page_shpkpr(char *name){
-    cout << "> Welcome " << name << " !\n";
+int home_page_shpkpr(Shopkeeper);
+
+int process(ulli option, Shopkeeper shopkeeper)
+{
+    switch (option)
+    {
+    case 1:
+    {
+        cout << ">Name : " << shopkeeper.name << endl;
+        cout << ">Email : " << shopkeeper.email << endl;
+        cout << ">contact number : " << shopkeeper.contact_number << endl;
+        cout << ">Account Number : " << shopkeeper.account_name << endl;
+        cout << ">IFSC code : " << shopkeeper.IFSC_code << endl;
+        cout << ">Pending amount : " << shopkeeper.pending_amount << endl;
+        cout << endl;
+        cout << ">select any of the following options :\n";
+        cout << ">a)Edit profile\n";
+        cout << ">b)go to home page\n";
+        cout << ">c)logout\n";
+        cout << ">";
+        char option1;
+        cin >> option1;
+        switch (option1){
+        case 'a':
+        {
+            shopkeeper.get_details(shopkeeper.email);
+            register_shopkeeper(shopkeeper, shopkeeper.email);
+            cout << ">profile updated successfully\n";
+            return home_page_shpkpr(shopkeeper);
+        }
+        case 'b':
+        {
+            system("clear");
+            home_page_shpkpr(shopkeeper);
+        }
+        case 'c':
+        {
+            // system("g++ main.cpp -o main && ./main");
+            return -1;
+        }
+        }
+    }
+    case 2:
+    {
+        
+    }
+    }
+}
+
+int home_page_shpkpr(Shopkeeper shopkeeper){
+    cout << "> Welcome " << shopkeeper.name << " !\n";
     cout << "\n";
     cout << ">Select any of the following options (Type the corresponding index to select a option):\n\n";
     cout << ">1)profile\n";
     cout << ">2)view items in the shop\n";
     cout << ">3)add items to shop\n";
     cout << ">4)transaction history\n";
+    cout<<">5)Logout\n";
     cout << "\n>";
+    ulli option; //contains the input option given by the user
+    cin >> option;
+    system("clear");
+    process(option,shopkeeper);
 }
+
+
 
 int shopkeeper_portal(string email)
 {
@@ -32,53 +89,7 @@ int shopkeeper_portal(string email)
         return -1;
     }
     
-    home_page_shpkpr(shopkeeper.name);
-
-    ulli option;   //contains the input option given by the user
-    cin>>option;
-    system("clear");
-
-    switch(option){
-        case 1:
-        {
-            cout<<">Name : "<<shopkeeper.name<<endl;
-            cout<<">Email : "<<shopkeeper.email<<endl;
-            cout<<">contact number : "<<shopkeeper.contact_number<<endl;
-            cout<<">Account Number : "<<shopkeeper.account_name<<endl;
-            cout<<">IFSC code : "<<shopkeeper.IFSC_code<<endl;
-            cout<<">Pending amount : "<<shopkeeper.pending_amount<<endl;
-            cout<<endl;
-            cout<<">select any of the following options :\n";
-            cout<<">a)Edit profile\n";
-            cout<<">b)go to home page\n";
-            cout<<">c)logout\n";
-            cout<<">";
-            char option1;
-            cin>>option1;
-            switch(option1)
-                case 'a':
-                {
-                    shopkeeper.get_details(shopkeeper.email);
-                    register_shopkeeper(shopkeeper,shopkeeper.email);
-                    cout<<">profile updated successfully\n";
-                    home_page_shpkpr(shopkeeper.name);
-                }
-                case 'b':
-                {
-                    system("clear");
-                    home_page_shpkpr(shopkeeper.name);
-                }
-                case 'c':
-                {
-                    return -1;
-                }
-
-        }
-        case 2:
-        {
-            
-        }
-    }
+    return home_page_shpkpr(shopkeeper);
 }
 #endif
 

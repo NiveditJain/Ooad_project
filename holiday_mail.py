7ecdd8b5a1dc286cccc3dd38d8c6b70f0f3d7bab
MuskanBathla <bathla.1@iitj.ac.in>
2019-11-24 00:04:35 +0530

    started moderator

---
+++ holiday_mail.py
@@ -0,0 +1,28 @@
+import smtplib
+import ssl
+import sys
+
+try:
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = "ccbasketiitjodhpur@gmail.com"
+    receiver_email = sys.argv[1]+"@iitj.ac.in"
+    password = "NiveditJain@ooad"
+    message = """\
+FROM: CC BASKET
+TO: """+receiver_email+"""
+Subject: Holiday announcement from CC Basket\n
+Dear """+sys.argv[3]+"\n"+"CC Basket will remain closed from "+"\n""Your OTP is "+sys.argv[2]
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email,receiver_email, message)
+    with open(".junk","w") as file:
+        file.write(str(1))
+except:
+    with open(".junk","w") as file:
+        file.write(str(-1))
--- moderator_portal.cpp
+++ moderator_portal.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
+#include "terminal.cpp"
 using namespace std;
 #include "moderator.cpp"
-int moderator_portal(string email)
+#include "customer.cpp"
+int moderator_auth()
 {
     string password;
-    
+    password=input_password();
     if(password.length()==0)
         return -1;
     
@@ -13,4 +15,38 @@ int moderator_portal(string email)
         cout<<"\n > invalid email password combination";
         return -1;
     }
+}
+
+void change_password()
+{
+    string passwrd;
+    cout<<"Enter new password:\n";
+    passwrd=input_password();
+    moderator.set_password(passwrd);
+}
+
+void assign_delivery_boy(Customer cs)
+{
+
+}
+
+void holiday()
+{
+    string start_date,end_date;
+    cout<<">Enter all dates in dd/mm/yyyy format"<<endl;
+    cout<<"Start date:"<<endl;
+    getline(cin,start_date);
+    cout<<"End date:"<<endl;
+    getline(cin,end_date);
+    char sdate[start_date.length()],edate[end_date.length()];
+    strcpy(sdate,start_date.c_str());
+    strcpy(edate,end_date.c_str());
+    if(strcmp(sdate,edate)==0)
+    else
+    {
+        
+    }
+    
+
+
 }

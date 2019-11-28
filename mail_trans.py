d419906025266fbd8f6bdd1c2db1f98eccfa87b7
NiveditJain <niveditjain@outlook.com>
2019-11-29 02:44:23 +0530

    restored rohith

--- mail_trans.py
+++ mail_trans.py
@@ -12,7 +12,7 @@ try:
 FROM: CC BASKET
 TO: """+receiver_email+"""
 Subject: Order Summary from CC Basket\n
-Dear """+sys.argv[2]+"\n"+"Your order has been successfully placed."+"\n""Which cost "+sys.argv[3]+" Rupees\n"+"Will be delived to you by "+sys.argv[4]+"(Phone number "+sys.argv[5]+")"+"\n"+"Your order is with mode "+sys.argv[6]+"\n"+"For further details you can contact moderator by reaching community cetre office or by replaying to this thread"+"\n"+"Thank Your for shoping with us"
+Dear """+sys.argv[2]+"\n"+"Your order has been successfully placed "+",which costs "+sys.argv[3]+" Rupees. "+"It will be delived to you by "+sys.argv[4]+"(Phone number "+sys.argv[5]+")."+"\n"+"Your order payment mode is "+sys.argv[6]+".\n"+"For further details you can contact moderator by reaching community cetre office or by replaying to this thread"+"\n"+"Thank Your for shoping with us"
 
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:

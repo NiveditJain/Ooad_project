5ea8feedbcf7b682856f670442166b3e782a8bfd
MuskanBathla <bathla.1@iitj.ac.in>
2019-11-28 21:58:39 +0530

    minor commit

--- moderator_portal.cpp
+++ moderator_portal.cpp
@@ -1,50 +1,45 @@
 #include<bits/stdc++.h>
 #include "terminal.cpp"
 using namespace std;
+
 #include "moderator.cpp"
 #include "customer.cpp"
 int moderator_auth()
 {
     string password;
-    password=input_password();
+    password=Terminal::input_password();
     if(password.length()==0)
         return -1;
     
-    if(moderator.login(password)==false)
+    if(Moderator::login(password)==false)
     {
         cout<<"\n > invalid email password combination";
         return -1;
     }
 }
 
-void change_password()
-{
-    string passwrd;
-    cout<<"Enter new password:\n";
-    passwrd=input_password();
-    moderator.set_password(passwrd);
-}
 
-void assign_delivery_boy(Customer cs)
+void holiday_vacation()
 {
-
-}
-
-void holiday()
-{
-    string start_date,end_date;
+    string start_date,end_date,occassion;
     cout<<">Enter all dates in dd/mm/yyyy format"<<endl;
     cout<<"Start date:"<<endl;
     getline(cin,start_date);
     cout<<"End date:"<<endl;
     getline(cin,end_date);
-    char sdate[start_date.length()],edate[end_date.length()];
+    cout<<"Enter the occassion:"<<endl;
+    getline(cin,occassion);
+    char sdate[start_date.length()],edate[end_date.length()],occ[occassion.length()];
     strcpy(sdate,start_date.c_str());
     strcpy(edate,end_date.c_str());
+    strcpy(occ,occassion.c_str());
     if(strcmp(sdate,edate)==0)
+    {
+        Moderator::mail_holiday(sdate,occ);
+    }
     else
     {
-        
+        Moderator::mail_vacation(sdate,edate,occ);
     }
     
 
